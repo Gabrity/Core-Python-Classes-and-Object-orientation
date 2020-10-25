da18ac76bdc62c1830e01599e225071e383d3a5b
Gabrity, G. (Gabor) <mp26od@INSIM.BIZ>
2020-10-25 20:54:56 +0100

    added lesson 7

--- Module_2/7_PropertiesAndInheritance/shipping.py
+++ Module_2/7_PropertiesAndInheritance/shipping.py
@@ -3,6 +3,9 @@ import iso6346
 class ShippingContainer:
 
     next_serial = 1337
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+    FRIDGE_VOLUME_FT3 = 100
 
     @classmethod
     def _generate_serial(cls):
@@ -18,27 +21,32 @@ class ShippingContainer:
         )
 
     @classmethod
-    def create_empty(cls, owner_code, **kwargs):
-        return cls(owner_code, contents = [], **kwargs)
+    def create_empty(cls, owner_code, length_ft, **kwargs):
+        return cls(owner_code, length_ft, contents = [], **kwargs)
 
     @classmethod
-    def create_with_items(cls, owner_code, items, **kwargs):
-        return cls(owner_code, contents = list(items), **kwargs)
+    def create_with_items(cls, owner_code, length_ft, items, **kwargs):
+        return cls(owner_code, length_ft, contents = list(items), **kwargs)
 
-    def __init__(self, owner_code, contents, **kwargs):
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.contents = contents
         self.bic = self._make_bic_code(
             owner_code = owner_code,
             serial = ShippingContainer._generate_serial()
         )
+
+    @property
+    def volume_ft3(self):
+        return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
         
 class RefigiratedShippingContainer(ShippingContainer):
     
     MAX_CELSIUS = 4.0
 
-    def __init__(self, owner_code, contents, *, celsius, **kwargs):
-        super().__init__(owner_code, contents, **kwargs)
+    def __init__(self, owner_code, length_ft, contents, *, celsius, **kwargs):
+        super().__init__(owner_code, length_ft, contents, **kwargs)
         self.celsius = celsius
 
     @staticmethod
@@ -67,6 +75,10 @@ class RefigiratedShippingContainer(ShippingContainer):
     def farenheit(self, value):
         self.celsius = RefigiratedShippingContainer._f_to_c(value)
 
+    @property
+    def volume_ft3(self):
+        return (super().volume_ft3 - RefigiratedShippingContainer.FRIDGE_VOLUME_FT3)
+
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(
@@ -74,13 +86,25 @@ class RefigiratedShippingContainer(ShippingContainer):
             serial = str(serial).zfill(6),
             category='R'
         )
+        
+
+class HeatedRefigiratedShippingContainer(RefigiratedShippingContainer):
+    
+    MIN_CELSIUS = -20
+
+    @RefigiratedShippingContainer.celsius.setter
+    def celsius(self, value):
+        if value < HeatedRefigiratedShippingContainer.MIN_CELSIUS:
+            raise ValueError('Too cold!')
+        RefigiratedShippingContainer.celsius.fset(self, value)
 
 
-container = RefigiratedShippingContainer.create_with_items("YML", ['fish'], celsius = -2)
-print(container.celsius)
-print(container.farenheit)
-container.celsius = -5
-print(container.celsius)
-print(container.farenheit)
-container.farenheit = 150
+container = ShippingContainer.create_empty("YML", 20)
+print(container.volume_ft3)
+
+container = RefigiratedShippingContainer.create_empty("YML", 20, celsius = -10)
+print(container.volume_ft3)
+
+container = HeatedRefigiratedShippingContainer.create_empty("YML", 20, celsius = -20)
+print(container.volume_ft3)
 
